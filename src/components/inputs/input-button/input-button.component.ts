d1fb89492de4b9e6aa92a4c0bbf0f25e8608f70a
eric9ten <eric@9ten-interactive.com>
2025-04-23 23:25:45 -0700

    ref: update add team form gender group

---
+++ src/components/inputs/input-button/input-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pkm-input-button',
+  imports: [],
+  templateUrl: './input-button.component.html',
+  styleUrl: './input-button.component.scss'
+})
+export class InputButtonComponent {
+
+}
